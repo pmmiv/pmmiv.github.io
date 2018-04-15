7ff6a8e429fb8112d7424c8f09e1b982782edc1a
PMMIV <pmmilamiv@gmail.com>
2018-04-15 14:14:41 -0500

    added background shift, width is broken and footer isn't showing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,23 @@
-$(document).ready(
-	console.log ("linked");
+var onScrollHandler = function() {
+  // var newImageUrl = yourImageElement.src;
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  if (scrollTop > 50) {
+    console.log("fired");
+    $("#readOn").hide();
+     // newImageUrl = "img1.jpg"
+  }
+  // if (scrollTop > 200) {
+  //    newImageUrl = "img2.jpg"
+  // }
+  // if (scrollTop > 300) {
+  //    newImageUrl = "img3.jpg"
+  // }
+  // yourImageElement.src = newImageUrl;
+};
 
-);
+
+$( document ).ready(function() {
+  console.log("ready")
+
+	window.addEventListener ("scroll", onScrollHandler);
+});
