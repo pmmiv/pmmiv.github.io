fb2d4a5454b9af4ab577b142af7be878841b60f9
unknown <pmmilamiv@gmail.com>
2018-04-15 21:41:08 -0500

    added slick and other stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ var onScrollHandler = function() {
   // var newImageUrl = yourImageElement.src;
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   if (scrollTop > 15) {
+    console.log("fired");
     $("#readOn").hide();
      // newImageUrl = "img1.jpg"
   }
@@ -14,9 +15,41 @@ var onScrollHandler = function() {
   // yourImageElement.src = newImageUrl;
 };
 
-
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $("nav").fadeIn(2500);
+
+  $('.workSlider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
 
 	window.addEventListener ("scroll", onScrollHandler);
 
