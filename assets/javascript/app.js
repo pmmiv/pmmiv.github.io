01c9da24a932d3e014027e47c6e92549d21fe43b
PMMIV <pmmilamiv@gmail.com>
2018-04-27 11:15:44 -0500

    finished up quote animation loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,56 @@
-$(document).ready(function() {
-	$(".mainContent").hide();
-	
+var speakers = [
+	{
+		"name": "Allen Iverson, Basketball",
+		"quote": "This guy gets it."
+	}, {
+		"name": "Bono, U2",
+		"quote": "A real class act, and an inspiration."
+	}, {
+		"name": "John Claybrook, Operations Manager",
+		"quote": "Prentice is a natural leader, and quality teacher. A phenomenal hire for anyone."
+	}, {
+		"name": "Charles Erdmann, Production Manager",
+		"quote": "PM is really dependable. He will always get the job done."
+	}, {
+		"name": "Lokesh Anand, Back-End Developer",
+		"quote": "He has an eye for detail. His persistence and problem solving skills make him a dependable team member."
+	}
+]
 
-	$(".enterSite").click(function() {
-		$(".cover").hide();
-		$(".enterSite").hide();
-		$(".mainContent").show();
+var curInd = 0;
+
+var quoteLoop = function () {
+	$(".quote").animate({opacity: "0"}, 500);
+	$(".speaker").animate({opacity: "0"}, 500, function(){
+		$(".quote").text(speakers[curInd].quote);
+		$(".speaker").text(speakers[curInd].name)
 	});
+	$(".quote").animate({opacity: "1"}, 500);
+	$(".speaker").animate({opacity: "1"}, 500);
+	if (curInd < speakers.length-1) {
+		curInd++;
+	} else {
+		curInd = 0;
+	}
+}
+
+$(document).ready(function() {
+	$(".enterSite").click(function() {
+		$(".titles").animate({opacity: "0"}, 500);
+		$("#coverHeadshot").animate({opacity: "0"}, 500);
+		$(".enterSite").animate({opacity: "0"}, 1000);
+		$(".cover").animate({right: "100%", width: "0", opacity: "0"}, 2000, function () {
+			$('.mainContent').animate({opacity: "1"}, 1000);
+			$('.cover').hide();
+			$(".enterSite").hide();
+		});
+	});
+
+	setInterval(quoteLoop, 10000);
+
+	$(".pageLink").click(function () {
+		$(".pageLink").removeClass("active");
+		$(this).addClass("active");
+	})
 
 });
